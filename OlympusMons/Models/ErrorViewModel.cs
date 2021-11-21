a7a4bf3b5f89c5d420fa8088a00c1bfa4b25aa81
owengovindasamy <owenagb@gmail.com>
2021-11-21 12:44:04 +0200

    Add project files.

---
+++ OlympusMons/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OlympusMons.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
